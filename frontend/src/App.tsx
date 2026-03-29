1c217e0a6c8e3919fa304a8a209822b9ed9f2b65
Untitled Publishers <mrmicaiah@gmail.com>
2026-03-29 12:08:02 -0500

    Trigger redeploy - bump version

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { AuthProvider } from './contexts/AuthContext';
 import { RouterConfig } from './router';
 
+// MediVault - Home Care Agency Management Platform
+// v1.0.1
+
 function App() {
   return (
     <AuthProvider>
